b195fed092ee0dd45c629b83b84d07ba9022b047
Paolo Cortis <paolo.cortis@gmail.com>
2010-04-07 14:03:05 +0000

    Improvements for expressions
    git-svn-id: https://phpbobot.googlecode.com/svn/trunk@54 e0c34714-8ca3-11dd-a9db-710176c1da70

--- Nuovobot/functions/expressions/expression.php
+++ Nuovobot/functions/expressions/expression.php
@@ -2,14 +2,21 @@
 	function expression($socket, $channel, $sender, $msg, $infos)
 	{
 		global $translations;
+		$command = "functions/expressions/espr";
 
 		$espr = $infos[1];
 
-		if(is_bot_op($sender))
-			$risultato = exec("functions/expressions/espr \"$espr\" 1");
-		else
-			$risultato = exec("functions/expressions/espr \"$espr\" 0");
+		$regex = "/^[\(]*[0-9]+([\+\-\*\/%\]{1}[\(]*[0-9]+[\)]*)+[\)]*$/";
 
-		sendmsg($socket, sprintf($translations->bot_gettext("expressions-result-%s-%s"), $espr, $risultato), $channel); //"Il risultato di $espr &egrave; $risultato"
+		if(preg_match($regex, $espr)) {
+			if(is_bot_op($sender))
+				$risultato = exec("{$command} \"{$espr}\" 1");
+			else
+				$risultato = exec("{$command} \"{$espr}\" 0");
+			
+			sendmsg($socket, sprintf($translations->bot_gettext("expressions-result-%s-%s"), $espr, $risultato), $channel); //"Il risultato di $espr &egrave; $risultato"
+		} else {
+			sendmsg($socket, "Ti sembra una espressione valida? Segui questa regex: {$regex}", $channel);
+		}
 	}
 ?>
