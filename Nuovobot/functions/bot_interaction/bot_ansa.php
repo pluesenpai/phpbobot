6bbcaa335d41093cb4660cda02c1d8de4d378c28
Paolo Cortis <paolo.cortis@gmail.com>
2010-04-29 17:55:51 +0000

    some changes
    git-svn-id: https://phpbobot.googlecode.com/svn/trunk@55 e0c34714-8ca3-11dd-a9db-710176c1da70

--- Nuovobot/functions/bot_interaction/bot_ansa.php
+++ Nuovobot/functions/bot_interaction/bot_ansa.php
@@ -6,7 +6,7 @@ function bot_ansa($socket, $channel, $sender, $msg, $infos)
 	if($infos[1] != "PRIVMSG" || $channel != "#bottoli")
 		return;
 
-	if(preg_match("/ANSA/", $msg))
+	if(preg_match("/ANSA|^ /", $msg) && $sender == "Bender")
 		sendmsg($socket, $msg, "#sardylan");
 }
 
