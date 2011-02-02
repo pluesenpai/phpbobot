439775f3d008996e25544c7560969d62db8b23f6
Paolo Cortis <paolo.cortis@gmail.com>
2011-02-02 18:38:11 +0000

    #49 [modes] Non avvisa se l'utente non esiste
    git-svn-id: https://phpbobot.googlecode.com/svn/trunk@70 e0c34714-8ca3-11dd-a9db-710176c1da70

--- Nuovobot/functions/utils/modes.php
+++ Nuovobot/functions/utils/modes.php
@@ -1,12 +1,28 @@
 <?php
 	function modes($socket, $channel, $sender, $msg, $infos)
 	{
-		global $registered, $auth, $db;
+		global $registered, $auth, $db, $users, $user_name;
 
 		if(!$registered[$sender] && !$auth[$sender]) {
 			sendmsg($socket, "Tu vuoi cosa? Per fare che?", $channel);
 			return;
 		}
+
+		$_users = explode(" ", $infos[1]);
+		$userscount = count($_users);
+
+		for($i = 0; $i < $userscount; $i++) {
+			$user = $_users[$i];
+			if(!preg_match("/\b([\+%&$~\@])*{$user}\b/", implode(" ", $users[$channel]))) {
+				sendmsg($socket, "$user non c'è!!", $channel);
+				unset($_users[$i]);
+			}
+		}
+
+		$_users = array_values($_users);
+		$userscount = count($_users);
+		if($userscount <= 0)
+			return;
 
 		$iduser = $db->check_user($sender);
 		$idchan = $db->check_chan($channel);
@@ -17,14 +33,14 @@
 		else
 			$mode = "+";
 		if(preg_match("/halfop (.+)$/", $infos[0]) && (preg_match("/h|o/", $result[0]["modes"]) || getUserPrivileges($sender, $channel) <= UserLevels::HALFOP_LEVEL || is_bot_op($sender)))
-			$mode .= str_repeat("h", substr_count($infos[1], " ") + 1);
+			$mode .= str_repeat("h", $userscount);
 		elseif(preg_match("/voice (.+)$/", $infos[0]) && preg_match("/v|h|o/", $result[0]["modes"] || is_bot_op($sender)))
-			$mode .= str_repeat("v", substr_count($infos[1], " ") + 1);
+			$mode .= str_repeat("v", $userscount);
 		elseif(preg_match("/o/", $result[0]["modes"]) || getUserPrivileges($sender, $channel) <= UserLevels::OPER_LEVEL || is_bot_op($sender))
-			$mode .= str_repeat("o", substr_count($infos[1], " ") + 1);
+			$mode .= str_repeat("o", $userscount);
 
 		if(strlen($mode) > 1) {
-			send($socket, "MODE $channel $mode {$infos[1]}\n");
+			send($socket, "MODE $channel $mode " . implode(" ", $_users) . "\n");
 		} else {
 			sendmsg($socket, "Tu vuoi cosa? Per fare che?", $channel);
 		}
