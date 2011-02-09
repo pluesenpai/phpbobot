a5f52139c422adb83597b062b215661cd822f7aa
Paolo Cortis <paolo.cortis@gmail.com>
2011-02-09 10:52:24 +0000

    #52 Controllare le funzioni builtins
    git-svn-id: https://phpbobot.googlecode.com/svn/trunk@72 e0c34714-8ca3-11dd-a9db-710176c1da70

--- Nuovobot/functions/builtins/greetuser.php
+++ Nuovobot/functions/builtins/greetuser.php
@@ -4,7 +4,7 @@
 		global $users, $user_name;
 	
 		if(preg_match("/\b([\+%&$~\@])*{$infos[1]}\b/", implode(" ", $users[$channel])) && $infos[1] != $user_name)
-			sendmsg($socket, sprintf(_("greetuser-message-%s", $infos[1])), $channel);
+			sendmsg($socket, sprintf(_("greetuser-message-%s"), $infos[1]), $channel);
 		elseif(count($infos) > 1 && $infos[1] == $user_name)
 			sendmsg($socket, sprintf(_("greetuser-userbot-%s"), $infos[1]), $channel);
 		else
