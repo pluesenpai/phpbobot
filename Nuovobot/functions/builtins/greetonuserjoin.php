427ffc0838e54aee623fb966a8ee44bd78d4810e
Paolo Cortis <paolo.cortis@gmail.com>
2011-02-13 15:31:02 +0000

    #31 funzioni per abilitare e disabilitare i saluti dei canali
    git-svn-id: https://phpbobot.googlecode.com/svn/trunk@76 e0c34714-8ca3-11dd-a9db-710176c1da70

--- Nuovobot/functions/builtins/greetonuserjoin.php
+++ Nuovobot/functions/builtins/greetonuserjoin.php
@@ -6,20 +6,33 @@
 
 		$cond_f = array("user_IDUser", "chan_IDChan");
 		$cond_o = array("=", "=");
-		$cond_v = array($db->find_user($sender), $db->find_chan($channel));
-		if(mb_strtoupper($infos[1]) == "ON") {
-			$value = "TRUE";
-		} else if(mb_strtoupper($infos[1]) == "OFF") {
-			$value = "FALSE";
+
+		if(isset($infos[3]) && is_bot_op($sender) && ($registered[$sender] || $auth[$sender]) {
+			$user = $infos[2];
+			$value = $infos[3];
 		} else {
-			$errore = 1;
+			$user = $sender;
+			$value = $infos[1];
 		}
 
-		if($errore != 1) {
-			$db->update("enter", array("cangreet"), array($value), $cond_f, $cond_o, $cond_v);
-			sendmsg($socket, "Tutto ok!", $channel);
+		if(is_user_in_chan($user)) {
+			$cond_v = array($db->find_user($user), $db->find_chan($channel));
+			if(mb_strtoupper($value) == "ON") {
+				$val = "TRUE";
+			} else if(mb_strtoupper($value) == "OFF") {
+				$val = "FALSE";
+			} else {
+				$errore = 1;
+			}
+
+			if($errore != 1) {
+				$db->update("enter", array("cangreet"), array($val), $cond_f, $cond_o, $cond_v);
+				sendmsg($socket, "Tutto ok!", $channel);
+			} else {
+				sendmsg($socket, "Errore", $channel);
+			}
 		} else {
-			sendmsg($socket, "Errore", $channel);
+			sendmsg($socket, "Chi????? Non è in canale!", $channel);
 		}
 	}
 ?>
